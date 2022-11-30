b62b012510e8495637be12a473c2bdcde509d045
Rupert Coghlan <r.coghlan@gmail.com>
2022-11-30 20:32:38 +0000

    adding selenium zoopla challenge

--- milestone_3_practicals.py
+++ milestone_3_practicals.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 import re
 from bs4 import NavigableString
+import lxml
 
 def surrounded_by_strings(tag):
     return (isinstance(tag.next_element, NavigableString)
@@ -30,27 +31,175 @@ soup.prettify()
 # %%
 soup.title
 # %%
-soup.title.name
+soup.title.name  
 # %%
 
 # %%
+# isloate the section where the actors information is found via the developer tab
 title_cast = soup.find('section', attrs={'data-testid': 'title-cast'})
 # %%
 print(title_cast)
 # %%
+## within the section above find the tags containsing the actors names
 actors = title_cast.find_all("a", attrs={"data-testid":"title-cast-item__actor"})
+
+actors_test = soup.find_all("a", attrs={"class":"sc-bfec09a1-1 gfeYgX"})
+
+# %%
+print(actors_test)
+print(actors)
 # %%
 print(actors[0].text)
 # %%
 for i in range(len(actors)):
     print(actors[i].text)
 # %%
+
+# isolate the section where the li
 actor_links = title_cast.find_all("a", attrs={"data-testid":"title-cast-item__actor"})
 # %%
 # %%
+## load the names and links in to a dict - have to use enumerate as you need to specify an int when slicing
 actor_dict = {}
 for index, link in enumerate(actor_links):
     actor_dict[actor_links[index].text] = str('https://www.imdb.com/' + actor_links[index]['href'])
 # %%
 actor_dict
+# %%
+
+## For each actor - navigate to the actors page and get the list of films they have satrred in
+
+## isolate the section where the film list is found
+travolta_url = 'https://www.imdb.com/name/nm0000237/'
+travolta_response = requests.get(travolta_url, headers={'referer': 'https://www.google.com/'})
+travolta_html = travolta_response.text
+travolta_soup = BeautifulSoup(travolta_html, 'lxml')
+
+# %%
+travolta_soup
+
+# %%
+travolta_soup.title
+# %%
+travolta_films = travolta_soup.find('div', class_ = 'ipc-metadata-list-summary-item__tc')
+print(travolta_films)
+# %%
+trav_films = travolta_soup.find_all(text="ipc-metadata-list-summary-item__t")
+# %%
+print(trav_films)
+# %%
+######### SELENIUM ###########
+
+# %%
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+driver = webdriver.Chrome()
+import time
+
+def load_and_accept_cookies() -> webdriver.Chrome:
+    '''
+    Open Zoopla and accept the cookies
+    
+    Returns
+    -------
+    driver: webdriver.Chrome
+        This driver is already in the Zoopla webpage
+    '''
+    driver = webdriver.Chrome() 
+    URL = "https://www.zoopla.co.uk/new-homes/property/london/?q=London&results_sort=newest_listings&search_source=new-homes&page_size=25&pn=1&view_type=list"
+    driver.get(URL)
+    time.sleep(3) 
+    try:
+        driver.switch_to_frame('gdpr-consent-notice') # This is the id of the frame
+        accept_cookies_button = driver.find_element(by=By.XPATH, value='//*[@id="save"]')
+        accept_cookies_button.click()
+        time.sleep(1)
+    except AttributeError: # If you have the latest version of Selenium, the code above won't run because the "switch_to_frame" is deprecated
+        driver.switch_to.frame('gdpr-consent-notice') # This is the id of the frame
+        accept_cookies_button = driver.find_element(by=By.XPATH, value='//*[@id="save"]')
+        accept_cookies_button.click()
+        time.sleep(1)
+
+    except:
+        pass
+
+    return driver 
+
+# %%
+def get_links(driver: webdriver.Chrome) -> list:
+    '''
+    Returns a list with all the links in the current page
+    Parameters
+    ----------
+    driver: webdriver.Chrome
+        The driver that contains information about the current page
+    
+    Returns
+    -------
+    link_list: list
+        A list with all the links in the page
+    '''
+
+    prop_container = driver.find_element(by=By.XPATH, value='//div[@class="css-1itfubx e1uxczc30"]')
+    prop_list = prop_container.find_elements(by=By.XPATH, value='./div')
+    link_list = []
+
+    for house_property in prop_list:
+        a_tag = house_property.find_element(by=By.TAG_NAME, value='a')
+        link = a_tag.get_attribute('href')
+        link_list.append(link)
+
+    return link_list
+
+# %%
+big_list = []
+driver = load_and_accept_cookies()
+
+## Get the next button
+next_property= driver.find_element(by=By.XPATH, value='//li[@class="css-qhg1xn-PaginationItemPreviousAndNext-PaginationItemNext eaoxhri2"]')
+next_tag = next_property.find_element(by=By.TAG_NAME, value='a')
+next_button = next_tag.get_attribute('href')
+
+
+"""
+With the new acquired knowledge, extract the data from all the properties in 5 different Zoopla pages. 
+This means that, once you finish scraping a page, you have to click the 'Next Page' button (you can also change the URL if you know how to tweak it). 
+So, once you extract the 25 links, you can go to the next page by clicking 'Next':
+
+"""
+# %%
+
+for i in range(5): # The first 5 pages only
+    big_list.extend(get_links(driver)) # Call the function we just created and extend the big list with the returned list
+    ## TODO: Click the next button. Don't forget to use sleeps, so the website doesn't suspect
+    time.sleep(3)
+    driver.get(next_button)
+
+# %%
+len(big_list)
+
+# %%
+dict_properties = {'Price': [], 'Address': [], 'Bedrooms': [], 'Description': []}
+for link in big_list:
+    driver.get(link)
+    price = driver.find_element(by=By.XPATH, value='//p[@data-testid="price"]').text
+    dict_properties['Price'].append(price)
+    address = driver.find_element(by=By.XPATH, value='//address[@data-testid="address-label"]').text
+    dict_properties['Address'].append(address)
+    bedrooms = driver.find_element(by=By.XPATH, value='//div[@class="c-PJLV c-PJLV-iiNveLf-css"]').text
+    dict_properties['Bedrooms'].append(bedrooms)
+    div_tag = driver.find_element(by=By.XPATH, value='//div[@data-testid="truncated_text_container"]')
+    span_tag = div_tag.find_element(by=By.XPATH, value='.//span')
+    description = span_tag.text
+    dict_properties['Description'] = description
+    time.sleep(3)
+
+# %%
+driver.quit() # Close the browser when you finish
+# %%
+driver.get(next_button)
+# %%
+big_list
+# %%
+dict_properties
 # %%
