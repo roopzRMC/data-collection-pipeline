02224c3f782265494770a9a35ba11b8832f588a2
Rupert Coghlan <r.coghlan@gmail.com>
2022-12-17 12:26:30 +0000

    added image writing and directory handling

--- milestone_4_practicals.py
+++ milestone_4_practicals.py
@@ -8,6 +8,9 @@ from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchElementException
 import time
 from datetime import datetime
+import urllib.request
+import os
+import json
 
 ## Determine which elements to scrape from individual hotel page
 # %%
@@ -21,8 +24,7 @@ images = images_container.find_elements(by=By.XPATH, value='//img[@class="Square
 # %%
 image_links = []
 for i in range(len(images)):
-    print(i)
-    image_links.append()
+    image_links.append(images[i].get_attribute('src'))
 # %%
 description_container = driver.find_element(by=By.XPATH, value='//div[@class="Box-sc-kv6pi1-0 leAmQV"]')
 
@@ -44,6 +46,9 @@ print(str(datetime.now()))
 # %%
 ## Function for hotel details
 def get_hotel_data(url):
+    ## Initialise the drivers for selenium
+    print(os.getcwd())
+
     driver = webdriver.Chrome()
     driver.get(url)
 
@@ -53,7 +58,23 @@ def get_hotel_data(url):
 
     ## Get name
     hotel_name = driver.find_element(by=By.XPATH, value = '//h1[@class="HeaderCerebrum__Name"]')
+    ## Check for file structure
+    raw_path = 'raw_data'
 
+    try:
+        os.makedirs(raw_path)
+    except:
+        FileExistsError
+        print('directory already exists')
+
+    os.chdir(raw_path)
+    ## Checks whether existing hotel exists within raw data and creates directory if not already there
+    try:
+        os.makedirs(hotel_name.text)
+    except:
+        FileExistsError
+        print('hotel directory already exists')
+    os.chdir(hotel_name.text)
 
     ## Get description
     #description_container = driver.find_element(by=By.XPATH, value='//div[@class="Box-sc-kv6pi1-0 leAmQV"]')
@@ -66,6 +87,16 @@ def get_hotel_data(url):
     for i in range(len(images)):
         image_links.append(images[i].get_attribute('src'))
 
+    ## Download the images
+    try:
+        os.makedirs('images')
+    except:
+        FileExistsError
+        print('image directory already exists')
+    os.chdir('images')
+    for i in range(len(image_links)):
+        urllib.request.urlretrieve(image_links[i].split('?', 1)[0], str(str(datetime.now()) + '.jpg'))
+
     
     ## Get facilities
     facilities = driver.find_elements(by=By.XPATH, value = '//div[@data-element-name="facility-highlights-item"]')
@@ -74,11 +105,13 @@ def get_hotel_data(url):
         facilities_list.append(facilities[j].text)
 
 
-    hotel_dict = {'Scrape Time': scrape_time, 'Hotel_Name': hotel_name.text, 'Images': image_links, 'Facilities' : facilities_list}
+    #hotel_dict = {'Scrape Time': scrape_time, 'Hotel_Name': hotel_name.text, 'Images': image_links, 'Facilities' : facilities_list}
 
-    return hotel_dict
+    #return hotel_dict
 # %%
 
+os.chdir('/Users/rupertcoghlan/data-collection-pipeline/')
+url='https://www.agoda.com/en-gb/century-plaza-hotel-spa_3/hotel/vancouver-bc-ca.html?finalPriceView=2&isShowMobileAppPrice=false&cid=1844104&numberOfBedrooms=&familyMode=false&adults=2&children=1&rooms=1&maxRooms=0&checkIn=2023-05-9&isCalendarCallout=false&childAges=7&numberOfGuest=0&missingChildAges=false&travellerType=2&showReviewSubmissionEntry=false&currencyCode=GBP&isFreeOccSearch=false&isCityHaveAsq=false&tspTypes=7,6,3&los=7&searchrequestid=efab119f-9b4e-4c5b-a9d1-eefe2b210837'
 myhotel = get_hotel_data(url)
 # %%
 myhotel
@@ -127,3 +160,22 @@ outfile.close()
 # %%
 str(hotel_name.text+'.json')
 # %%
+## 
+# Get an image
+
+import urllib.request
+
+img_url = image_links[0].split('?', 1)[0]
+print(img_url)
+print(images[0].accessible_name)
+# %%
+os.getcwd()
+# %%
+## Store the image with datetime appended to the name
+urllib.request.urlretrieve(img_url, str(str(datetime.now()) + '.jpg'))
+
+
+# %%
+# Function to create file structure
+
+
